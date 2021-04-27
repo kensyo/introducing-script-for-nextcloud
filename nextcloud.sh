5eac7d3199abe6c5acf3ae4dfa0c9f0792cc2af2
kensyo <takitateokowa@gmail.com>
2021-04-27 09:08:51 +0900

    [UPDATE] Version update

--- nextcloud.sh
+++ nextcloud.sh
@@ -35,7 +35,7 @@ declare -r YML=${DOCKER_DIR}/docker-compose.yml
 
 declare -r GITHUB_BASE_URL="https://raw.githubusercontent.com/kensyo/introducing-script-for-nextcloud/master"
 
-declare -r VERSION=v7
+declare -r VERSION=v8
 
 echo "SCRIPT VERSION: ${VERSION}"
 
