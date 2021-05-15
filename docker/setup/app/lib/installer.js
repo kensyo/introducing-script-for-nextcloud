58724ab6fe3606bbaab98e5a5a04940e277e09df
kensyo <takitateokowa@gmail.com>
2021-05-16 06:47:58 +0900

    [UPDATE] Remove unnecessary comments and files

--- docker/setup/app/lib/installer.js
+++ docker/setup/app/lib/installer.js
@@ -7,20 +7,6 @@ module.exports = {
 // public
 function install() {
     createDockerComposeYmlForInstallation();
-
-    // const template = fs.readFileSync('files/config.yml.template', 'utf-8');
-    // const yml = util.template(
-    //     template,
-    //     YML_DEFAULTS
-    // );
-
-    // const NC_CONFIG_PATH = `${CONFIG['DATA_DIR']}/config.yml`;
-
-    // fs.writeFileSync(NC_CONFIG_PATH, yml, 'utf8');
-
-    // const dcc = require('./dockerConfsCreater');
-    // dcc.createDockerComposeYml();
-
 }
 
 // private
