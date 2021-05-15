3b6c83cfb73f047c001e6a5f99442c7eb6f36ddd
kensyo <takitateokowa@gmail.com>
2021-05-15 16:53:13 +0900

    [REFACTOR] Use objects instead of replacement

--- docker/setup/app/lib/installer.js
+++ docker/setup/app/lib/installer.js
@@ -29,45 +29,53 @@ function createDockerComposeYmlForInstallation() {
 
     console.info('Start setting.');
 
-    const replacementWords = {};
-    replacementWords['NEXTCLOUD_ADMIN_USER'] = rls.question(
+    const NEXTCLOUD_ADMIN_USER = rls.question(
         'Enter admin user name: ',
         {charlist: '$<a-z>$<A-Z>$<0-9>', min: 1, max: 16}
     );
-    replacementWords['NEXTCLOUD_ADMIN_PASSWORD'] = rls.questionNewPassword(
+    const NEXTCLOUD_ADMIN_PASSWORD = rls.questionNewPassword(
         'Enter admin user password: ',
         {charlist: '$<a-z>$<A-Z>$<0-9>!#%&()*+,-./;<=>?@[]^_{|}~', min: 1, max: 32}
     );
-    replacementWords['MYSQL_ROOT_PASSWORD'] = rls.questionNewPassword(
+    const MYSQL_ROOT_PASSWORD = rls.questionNewPassword(
         'Enter MYSQL root password: ',
         {charlist: '$<a-z>$<A-Z>$<0-9>!#%&()*+,-./;<=>?@[]^_{|}~', min: 1, max: 32}
     );
-    replacementWords['MYSQL_DATABASE'] = rls.question(
+    const MYSQL_DATABASE = rls.question(
         'Enter MYSQL database name: ',
         {charlist: '$<a-z>$<A-Z>$<0-9>', min: 1, max: 16}
     );
-    replacementWords['MYSQL_USER'] = rls.question(
+    const MYSQL_USER = rls.question(
         'Enter MYSQL user name: ',
         {charlist: '$<a-z>$<A-Z>$<0-9>', min: 1, max: 16}
     );
-    replacementWords['MYSQL_PASSWORD'] = rls.questionNewPassword(
+    const MYSQL_PASSWORD = rls.questionNewPassword(
         'Enter MYSQL user password: ',
         {charlist: '$<a-z>$<A-Z>$<0-9>!#%&()*+,-./;<=>?@[]^_{|}~', min: 1, max: 32}
     );
 
-    const fs = require('fs-extra');
-    const template = fs.readFileSync(
-        'files/installaion-docker-compose.yml.template',
-        'utf-8'
-    );
     const util = require('./utility');
-    const ymlText = util.template(
-        template,
-        replacementWords
-    );
+    const base = util.loadYml('files/base-docker-compose.yml');
+
+    base['services']['db']['environment'] = [
+        `MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}`,
+        `MYSQL_USER=${MYSQL_USER}`,
+        `MYSQL_PASSWORD=${MYSQL_PASSWORD}`,
+        `MYSQL_DATABASE=${MYSQL_DATABASE}`
+    ];
+
+    base['services']['app']['image'] = 'nextcloud';
+    const environment = base['services']['app']['environment'];
+    environment.push(`NEXTCLOUD_ADMIN_USER=${NEXTCLOUD_ADMIN_USER}`);
+    environment.push(`NEXTCLOUD_ADMIN_PASSWORD=${NEXTCLOUD_ADMIN_PASSWORD}`);
+    environment.push(`MYSQL_USER=${MYSQL_USER}`);
+    environment.push(`MYSQL_PASSWORD=${MYSQL_PASSWORD}`);
+    environment.push(`MYSQL_DATABASE=${MYSQL_DATABASE}`);
 
     const CONFIG = util.loadConfig();
     const DOCKER_DIR = CONFIG['DATA_DIR'] + '/' + CONFIG['DOCKER_DIR_RELATIVE_PATH'];
+    const fs = require('fs-extra');
+    const jsyml = require('js-yaml');
     fs.mkdirsSync(DOCKER_DIR);
-    fs.writeFileSync(DOCKER_DIR + '/docker-compose.yml', ymlText, 'utf8');
+    fs.writeFileSync(`${DOCKER_DIR}/docker-compose.yml`, jsyml.dump(base), 'utf8');
 }
